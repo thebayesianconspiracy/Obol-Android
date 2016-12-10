f29738139b2d92c4d9ad7c395e5f8e5dea8b3da6
Sreejith Pp <mailz4sreejith@gmail.com>
2016-12-10 16:00:11 +0530

    Covered all notification cases

--- index.android.js
+++ index.android.js
@@ -6,10 +6,12 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
+    AppRegistry,
+    TouchableOpacity,
     StyleSheet,
     TouchableHighlight,
     AsyncStorage,
+    DeviceEventEmitter,
     Alert,
     Text,
   View
@@ -19,8 +21,31 @@ import {HomeRoute} from './screens';
 import {listen, sendMessage} from './utils/util';
 import store from './utils/store';
 import { Provider } from 'react-redux';
+import Notification from 'react-native-system-notification';
+import PushNotification from 'react-native-push-notification';
+
+import PushNotificationAndroid from 'react-native-push-notification'
+
+(function() {
+  // Register all the valid actions for notifications here and add the action handler for each action
+  PushNotificationAndroid.registerNotificationActions(['Accept','Reject','Yes','No']);
+  DeviceEventEmitter.addListener('notificationActionReceived', function(action){
+    console.log ('Notification action received: ' + action);
+    const info = JSON.parse(action.dataJSON);
+    if (info.action == 'Accept') {
+      // Do work pertaining to Accept action here
+    } else if (info.action == 'Reject') {
+      // Do work pertaining to Reject action here
+    }
+    // Add all the required actions handlers
+  });
+})();
+
 
 function onStart() {
+    PushNotification.configure({
+	requestPermissions: true,
+    });
     listen(message => {
 	try {
 	    const data = JSON.parse(message);
@@ -37,7 +62,6 @@ function onStart() {
 //	Alert.alert("Message is", message)
     })
 }
-
 class Obol extends Component {
 
     constructor(){
@@ -52,7 +76,7 @@ class Obol extends Component {
 		    backButtonComponent={() => (<Text style={{color: 'white', padding: 5, fontSize: 12}}>Back</Text>)}
 		    firstRoute={HomeRoute}
 		    headerStyle={styles.header}
-	    />
+		/>
 	    </Provider>
 	);
     }
